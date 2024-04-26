765070e82718c2dfcbdfa5ba128d8dc5fb615ab8
tmakhanya023a <tmakhanya023a@student.wethinkcode.co.za>
2024-04-27 00:13:32 +0200

    on it

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,19 @@ repositories {
 }
 
 dependencies {
+
+    // https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java
+//    implementation("org.seleniumhq.selenium:selenium-java:4.18.1")
+    implementation("org.seleniumhq.selenium:selenium-java:4.9.0")
+
+    // https://mvnrepository.com/artifact/io.github.bonigarcia/webdrivermanager
+    implementation("io.github.bonigarcia:webdrivermanager:5.8.0")
+
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
+
 
 tasks.test {
     useJUnitPlatform()
